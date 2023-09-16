223c1d54c32e51409fbef2c241e1d1df355d3760
joycelin0w0 <joyce.lin.1@stonybrook.edu>
2023-09-16 19:15:42 +0000

    added rand page

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
+import random
+from faker import Faker
+
+faker = Faker()
 
 app = Flask(__name__)
 
@@ -10,6 +14,12 @@ def mainpage():
 @app.route('/about')
 def aboutpage():
         return render_template('about.html')
+
+@app.route('/random')
+def randomstuff():
+    number_var = random.randint(1,10000)
+    fake_address = faker.address()
+    return render_template('random.html', single_number = number_var, single_address = fake_address)
         
 if __name__ == '__main__':
     app.run(
